daab2f9f4d6486d22aba506fe7bb80e616bb0d17
Conor Hutchins <conorhutchins@btinternet.com>
2025-04-22 17:39:19 +0100

    add article data, make a type file for it, update gitignore to include package.lock

---
+++ src/data/articles.ts
@@ -0,0 +1,28 @@
+import { Article } from '../types/Article';
+
+export const articles: Article[] = [
+  {
+    id: '1',
+    title: 'First News Story',
+    summary: 'The Pope died today',
+    content: 'Really sadly the Pope died yesterday, it looks like he had pneumonia',
+    author: 'Priest Conor',
+    publicationDate: '2025-04-21',
+  },
+  {
+    id: '2',
+    title: 'Second News Story',
+    summary: 'Water is wet',
+    content: 'Yes that is right, you heard it here first water is wet',
+    author: 'Scientist Conor',
+    publicationDate: '2025-04-22',
+  },
+  {
+    id: '3',
+    title: 'Third News Story',
+    summary: 'Leeds United are promoted to the Premier League',
+    content: 'Daniel farke taken the team to the Premier League, Marching On Together',
+    author: 'Football Pundit Conor',
+    publicationDate: '2025-04-22',
+  },
+];
---
+++ src/types/Article.ts
@@ -0,0 +1,9 @@
+// This will be the structure of an article object
+export interface Article {
+  id: string;
+  title: string;
+  summary: string;
+  content: string;
+  author: string;
+  publicationDate: string;
+} 
